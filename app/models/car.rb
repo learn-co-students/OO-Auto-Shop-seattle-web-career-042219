acc78439206f34048df70608a76e1a39d18a9471
jkshareef <jamalkshareef@gmail.com>
2019-05-02 12:21:22 -0700

    Done.

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,11 +1,33 @@
 class Car
 
-  attr_reader :make, :model
+  @@all = []
+
+  attr_accessor :car_owner, :mechanic
+  attr_reader :make, :model, :classification
 
   def initialize(make, model, classification)
     @make = make
     @model = model
     @classification = classification
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.classifications
+    arr = []
+    self.all.map do |car|
+      arr << car.classification
+    end
+    arr.uniq
+  end
+
+  def mechanics
+    Mechanics.all.select do |mechanic|
+      mechanic.specialty == self.classification
+    end
   end
 
 end
--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -1,11 +1,40 @@
 class Mechanic
 
+  @@all = []
+
   attr_reader :name, :specialty
 
-def initialize(name, specialty)
-  @name = name
-  @specialty = specialty
-end
+  def initialize(name, specialty)
+    @name = name
+    @specialty = specialty
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def cars
+    Car.all.select do |car|
+      car.mechanic == self
+    end
+  end
+
+  def car_owners
+    arr = []
+    Car.all.select do |car|
+      if car.mechanic == self
+        arr << car.car_owner
+      end
+    end
+    arr
+  end
+
+  def car_owners_names
+    self.car_owners.map do |car_owner|
+      car_owner.name
+    end
+  end
 
 
 end
